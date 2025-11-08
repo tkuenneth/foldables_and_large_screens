e805a99145f1625587de5a165c345836550dda11
Thomas Künneth <kuennetht@gmail.com>
2025-11-08 14:28:52 +0100

    Using common resources

--- MaterialAdaptiveMultiplatformDemo/composeApp/src/desktopMain/kotlin/de/thomaskuenneth/material/adaptive/multiplatform/main.kt
+++ MaterialAdaptiveMultiplatformDemo/composeApp/src/desktopMain/kotlin/de/thomaskuenneth/material/adaptive/multiplatform/main.kt
@@ -2,11 +2,14 @@ package de.thomaskuenneth.material.adaptive.multiplatform
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import materialadaptivemultiplatformdemo.composeapp.generated.resources.Res
+import materialadaptivemultiplatformdemo.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "MaterialAdaptiveMultiplatformDemo",
+        title = stringResource(Res.string.app_name),
     ) {
         MaterialAdaptiveDemo()
     }
